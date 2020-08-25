8f57ea396c43bb9a7928d103a02a3f7b3f5b0a0a
Ivy Brain <ivymbrain@gmail.com>
2020-08-25 13:58:08 +1000

    Adds assignment and subject

--- src/com/ivy/Staff.java
+++ src/com/ivy/Staff.java
@@ -14,6 +14,6 @@ public class Staff extends Person {
 
         this.staffID = staffID;
 
-        
+
     }
 }
---
+++ src/com/ivy/Subject.java
@@ -0,0 +1,15 @@
+package com.ivy;
+
+public class Subject {
+
+
+    Assignment[] assignments;
+    String name;
+    String subjectCode;
+
+    public Subject(String name, String subjectCode) {
+        this.name = name;
+        this.subjectCode = subjectCode;
+    }
+
+}
